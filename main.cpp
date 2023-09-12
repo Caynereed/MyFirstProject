1df553de1344948d53f76d173834bde239a5d360
Caynereed <reed146@tritons.iowacentral.edu>
2023-09-12 10:46:26 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Cayne Reed on 9/12/23.
+// This is a comment made by me, the creator of the code. 
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!" <<std::endl;
+    return 0;
+}
